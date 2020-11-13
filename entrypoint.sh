2d68f1690ca4af9ccb7a80a98e9bdcf0a060d7f7
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-11-14 08:56:35 +1300

    fix: explicitly call git push --force

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,17 @@ eval `ssh-agent -s`
 ssh-add - <<< "${SSH_PRIVATE_KEY}"
 
 last_commit_log=$(git log -1 --pretty=format:"%s")
-use_force=""
-if [[ "${last_commit_log}" == *"[force]"* ]]; then
-  use_force="--force"
-fi
 
 # split single parameter of this script into multiple params for the command
 eval "set -- ${1}"
 git-filter-repo "${@}"
 
-git push "git@github.com:${TARGET_ORG}/${TARGET_REPO}.git" HEAD:"${TARGET_BRANCH}" "${use_force}"
+git_host="git@github.com:${TARGET_ORG}/${TARGET_REPO}.git"
+git_branch="HEAD:${TARGET_BRANCH}"
+
+if [[ "${last_commit_log}" == *"[force]"* ]]
+then
+  git push --force "${git_host}" "${git_branch}"
+else
+  git push "${git_host}" "${git_branch}"
+fi
