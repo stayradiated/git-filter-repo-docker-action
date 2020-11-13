5e81969c58af1d16c0a5638b2417169adb0f8cdc
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-11-14 08:34:30 +1300

    fix: get last_commit_log manually

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@ ssh-keyscan -t rsa github.com >> /etc/ssh/ssh_known_hosts
 eval `ssh-agent -s`
 ssh-add - <<< "${SSH_PRIVATE_KEY}"
 
+local last_commit_log=$(git log -1 --pretty=format:"%s")
+local use_force=""
+if [[ "${last_commit_log}" == *"[force]"* ]]; then
+  use_force="--force"
+fi
+
 # split single parameter of this script into multiple params for the command
 eval "set -- ${1}"
 git-filter-repo "${@}"
-
-use_force=""
-if [[ "${LAST_COMMIT_MESSAGE}" == *"[force]"* ]]
-then
-  use_force="--force"
-fi
 
 git push "${use_force}" "git@github.com:${TARGET_ORG}/${TARGET_REPO}.git" HEAD:"${TARGET_BRANCH}"
